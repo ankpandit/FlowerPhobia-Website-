25e7477ce5339d7ca5ce359576c4b3433e299422
ankpandit <79821628+ankpandit@users.noreply.github.com>
2023-06-24 00:25:01 +0530

    Delete Flower webiste directory

--- Flower webiste/script.js
+++
@@ -1,8 +0,0 @@
-function signin(){
-    console.log("clicked !!!") ;
-    
-    let b =document.getElementById('frame') ;
-    b.style.cssText="position: absolute; visibility:visible; top: 40px; width: 100%; height: 100%; z-index: 10;" ;
-
-
-}
