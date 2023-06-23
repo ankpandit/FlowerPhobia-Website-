c8dc63c6f2a98ffd08bdddc20a09b76ed8c557e3
ankpandit <79821628+ankpandit@users.noreply.github.com>
2023-06-24 00:18:14 +0530

    Add files via upload

---
+++ Flower webiste/script.js
@@ -0,0 +1,8 @@
+function signin(){
+    console.log("clicked !!!") ;
+    
+    let b =document.getElementById('frame') ;
+    b.style.cssText="position: absolute; visibility:visible; top: 40px; width: 100%; height: 100%; z-index: 10;" ;
+
+
+}
